e086506ab1cedf863e652cecd35233990a2e1f22
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-08-19 22:28:55 -0400

    better bg

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,10 @@
 
 <template>
   <main class="bg--home irl flex-col justify-center text-center shadow-lg">
-    <h3
-      class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 mb-3 font-extrabold text-7xl"
-    >
+    <h3 class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 mb-3 font-extrabold text-7xl">
       Welcome to
     </h3>
-    <h1
-      class="text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-blue-500 mb-3 font-extrabold text-8xl pb-2"
-    >
+    <h1 class="text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-blue-500 mb-3 font-extrabold text-8xl pb-2">
       Programmatic
     </h1>
     <!-- <RouterLink to="/about"
@@ -20,14 +16,11 @@
         Why Us?
       </button></RouterLink
     > -->
-    <RouterLink to="/enroll"
-      ><button
+    <RouterLink to="/enroll"><button
         class="bg-gray-600 hover:bg-gray-500 active:bg-gray-600 p-3 rounded shadow-md font-bold text-blue-300 m-8"
-        role="link"
-      >
-        Get Started ->
-      </button></RouterLink
-    >
+        role="link">
+        Get Started &rarr;
+      </button></RouterLink>
     <!-- <RouterLink to="/volunteer"
       ><button
         class="bg-gray-600 hover:bg-gray-500 active:bg-gray-600 p-3 rounded shadow-md font-bold text-green-300 m-8"
@@ -44,11 +37,9 @@
     <hr class="mx-auto w-1/4 mb-12 border border-purple-700" />
     <section class="flex gap-56">
       <section class="h-1 flex flex-col items-center">
-        <img
-          class="img-style border rounded-sm border-black"
+        <img class="img-style border rounded-sm border-black"
           src="https://images.unsplash.com/photo-1623479322729-28b25c16b011?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-          alt="a picture of a person coding"
-        />
+          alt="a picture of a person coding" />
         <h4 class="text-3xl text-center text-white font-bold my-5">
           Programming Classes
         </h4>
@@ -62,21 +53,16 @@
           your passion for programming and open the door to endless
           opportunities.
         </p>
-        <RouterLink to="/enroll"
-          ><button
+        <RouterLink to="/enroll"><button
             class="bg-gray-600 hover:bg-gray-500 active:bg-gray-600 p-3 rounded shadow-md font-bold text-blue-300 m-8 flex-shrink-0"
-            role="link"
-          >
+            role="link">
             Enroll Now
-          </button></RouterLink
-        >
+          </button></RouterLink>
       </section>
       <section class="flex flex-col items-center">
-        <img
-          class="img-style border rounded-sm border-black"
+        <img class="img-style border rounded-sm border-black"
           src="https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-          alt="a picture of a person teaching someone"
-        />
+          alt="a picture of a person teaching someone" />
         <h4 class="text-3xl text-center text-white font-bold my-5">
           Volunteer oppotunity
         </h4>
@@ -87,14 +73,11 @@
           coding enthusiast, a tech-savvy mentor, or just someone with a heart
           for helping, we welcome you to join our volunteer team.
         </p>
-        <RouterLink to="/volunteer"
-          ><button
+        <RouterLink to="/volunteer"><button
             class="bg-gray-600 hover:bg-gray-500 active:bg-gray-600 p-3 rounded shadow-md font-bold text-green-300 m-8 flex-shrink-0"
-            role="link"
-          >
+            role="link">
             Volunteer
-          </button></RouterLink
-        >
+          </button></RouterLink>
       </section>
     </section>
   </article>
@@ -115,7 +98,7 @@
 
 <style scoped>
 .bg--home {
-  background-image: url("https://img.freepik.com/free-vector/gradient-technology-symbols-background_23-2149110134.jpg?w=2000&t=st=1692487959~exp=1692488559~hmac=cdbe3ea73784aae2df972d3ae7b6233691632f91c147c58ac124c0db674354f5");
+  background-image: url("https://images.unsplash.com/photo-1625535163131-9d1fc30ea5f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2664&q=80");
   background-repeat: no-repeat;
 }
 </style>
