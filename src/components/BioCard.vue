c55c6751b48ac9af47a351a1b0b3a72de293018e
Ofish <vc.ofir@gmail.com>
2023-08-19 22:06:27 -0400

    formatting-fix

--- src/components/BioCard.vue
+++ src/components/BioCard.vue
@@ -3,34 +3,48 @@ const props = defineProps({
   name: String,
   desc: String,
   langs: String,
-  thecs: String,
+  pos: String,
   discord: String,
-  imgurl: String
-})
-
+  imgurl: String,
+});
 
 function getImageUrl() {
-  return new URL(`../assets/${props.imgurl}`, import.meta.url)
+  return new URL(`../assets/${props.imgurl}`, import.meta.url);
 }
 </script>
 
- 
 <template>
   <div class="card flex border rounded border-black bg-gray-950 justify-center">
     <div class="content flex gap-4 justify-center">
-      <img class="w-20 h-20 p-3" :src="getImageUrl()" alt="a picture of a Programmatic member"> 
+      <img
+        class="w-20 h-20 p-3 mt-11"
+        :src="getImageUrl()"
+        alt="a picture of a Programmatic member"
+      />
       <section class="flex flex-col gap-2 justify-center">
         <h2 class="text-white font-bold">{{ name }}</h2>
+        <p class="text-white">{{ pos }}</p>
         <p class="text-white">{{ desc }}</p>
-        <p class="text-white">{{ thecs }}</p> 
         <p class="text-white">{{ langs }}</p>
         <p class="text-white">{{ discord }}</p>
-        <button class="bg-gray-600 hover:bg-gray-500 active:bg-gray-600 duration-700 p-3 rounded shadow-md font-bold text-blue-300 mr-8 mt-8">
+        <button
+          class="bg-gray-800 hover:bg-gray-700 active:bg-gray-600 duration-700 p-3 rounded shadow-md text-blue-100 mr-8 mt-8"
+        >
           <div class="flex justify-center items-center">
-            <p>Learn more</p> 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-              class="w-6 h-6"> 
-              <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
+            <p class="font-bold">Learn more</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-7 h-7"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+              ></path>
             </svg>
           </div>
         </button>
@@ -38,5 +52,3 @@ function getImageUrl() {
     </div>
   </div>
 </template>
-
-
--- src/views/OurTeamView.vue
+++ src/views/OurTeamView.vue
@@ -1,37 +1,59 @@
 <script setup>
-import BioCard from "../components/BioCard.vue"
+import BioCard from "../components/BioCard.vue";
 </script>
 
 <template>
-  <main class="flex gap-x-80 top-44"> 
-
-    <section class="fixed"> 
-      <h1 class="font-extrabold text-gray-200 text-6xl mt-32">Our Team: </h1>
-      <h3 class="text-lg font-bold py-10 ourTeam-desc">This is the team of founders, developers, and teachers behind Programmatic:
-         Our diverse and dynamic team is the driving force behind the continued development and improvment of Programmatic.
-        With a perfect blend of visionary founders, 
-        brilliant developers, and creative minds, our team has a passion for providing excellent teaching material and classes for our students. 
-        We're united by our shared commitment 
-        to improving Programmatic and creating impactful experiences for our students. Meet the team: </h3> 
+  <main class="flex gap-x-80 top-44">
+    <section class="fixed">
+      <h1 class="font-extrabold text-gray-200 text-6xl mt-32">Our Team:</h1>
+      <h3 class="text-lg font-bold py-10 ourTeam-desc text-gray-300">
+        This is the team of founders, developers, and teachers behind
+        Programmatic: Our diverse and dynamic team is the driving force behind
+        the continued development and improvment of Programmatic. With a perfect
+        blend of visionary founders, brilliant developers, and creative minds,
+        our team has a passion for providing excellent teaching material and
+        classes for our students. We're united by our shared commitment to
+        improving Programmatic and creating impactful experiences for our
+        students. Meet the team:
+      </h3>
     </section>
-    <hr class="px-20"> 
-    <div class="border border-gray-600"></div> 
-    <section class="flex flex-col gap-20 mt-36 mb-10"> 
-      <BioCard name="Ofir"
+    <hr class="px-20" />
+    <div class="border border-gray-600"></div>
+    <section class="flex flex-col gap-20 mt-36 mb-10">
+      <BioCard
+        name="Ofir"
+        pos="Founder"
+        desc=" Hi, I'm Ofir and im the founder of Programmatics. I have over 5 years of coding experience in Python and recently started working with Unity and dart."
+        langs="Python, Dart, C#"
+        discord="@.ofish"
+        imgurl="discord.png"
+      />
+      <BioCard
+        name="Zain"
         desc="1000 year old developer invented c++ hacked nsa with a potato invented the internet more important than allen turing bricked your mom's windows 98"
-        langs="Python" discord="@yourmom" thecs="hacking and kali linux" imgurl="discord.png" />
-      <BioCard name="Zain"
-        desc="1000 year old developer invented c++ hacked nsa with a potato invented the internet more important than allen turing bricked your mom's windows 98"
-        langs="Python" discord="@yourmom" thecs="hacking and kali linux" imgurl="discord.png" />
+        langs="Python"
+        discord="@yourmom"
+        pos="hacking and kali linux"
+        imgurl="discord.png"
+      />
 
-      <BioCard name="Adam"
+      <BioCard
+        name="Adam"
         desc="1000 year old developer invented c++ hacked nsa with a potato invented the internet more important than allen turing bricked your mom's windows 98"
-        langs="Python" discord="@yourmom" thecs="hacking and kali linux" imgurl="discord.png" />
+        langs="Python"
+        discord="@yourmom"
+        pos="hacking and kali linux"
+        imgurl="discord.png"
+      />
 
-      <BioCard name="Et"
+      <BioCard
+        name="Et"
         desc="1000 year old developer invented c++ hacked nsa with a potato invented the internet more important than allen turing bricked your mom's windows 98"
-        langs="Python" discord="@yourmom" thecs="hacking and kali linux" imgurl="discord.png" />
+        langs="Python"
+        discord="@yourmom"
+        pos="hacking and kali linux"
+        imgurl="discord.png"
+      />
     </section>
   </main>
 </template>
-
