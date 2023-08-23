3b78023321dea0e13a8dfe76ba7d7e279128595d
Ofish <vc.ofir@gmail.com>
2023-08-23 18:23:13 -0400

    about_fix

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -25,18 +25,19 @@
       </div>
     </section>
   </main>
-  <footer class="mt-52 flex gap-24 mb-10">
+  <footer class="mt-52 flex gap-24 mb-10 overflow-hidden">
     <section class="flex flex-col w-about-extra-custom">
       <h4 class="txt-custom text-3xl font-bold text-white">Future goals</h4>
       <p
         class="w-about-extra-custom txt-custom mt-2 mb-4 text-lg text-gray-300"
       >
-        In our pursuit of empowering prospective computer science students worldwide, our future goals
-        encompass expanding our organization, ensuring
-        accessibility, while also broadening the scope of
-        our curriculum to encompass a wider variety of programming concepts. By expanding, as well as incorporating a diverse range of
-        programming concepts into our curriculum, we aspire to cater to the
-        varied interests and career aspirations of our students.
+        In our pursuit of empowering prospective computer science students
+        worldwide, our future goals encompass expanding our organization,
+        ensuring accessibility, while also broadening the scope of our
+        curriculum to encompass a wider variety of programming concepts. By
+        expanding, as well as incorporating a diverse range of programming
+        concepts into our curriculum, we aspire to cater to the varied interests
+        and career aspirations of our students.
       </p>
     </section>
     <section class="flex flex-col w-about-extra-custom">
@@ -44,28 +45,26 @@
       <p
         class="w-about-extra-custom txt-custom mt-2 mb-4 text-lg text-gray-300"
       >
-        At the heart of our organization lies a mission: to nurture
-        digital literacy among students and empower them with essential skills
-        to not only navigate but thrive in an era fundamentally defined by
-        computer science. We are dedicated to fostering a generation of individuals who
+        At the heart of our organization lies a mission: to nurture digital
+        literacy among students and empower them with essential skills to not
+        only navigate but thrive in an era fundamentally defined by computer
+        science. We are dedicated to fostering a generation of individuals who
         can confidently embrace the digital landscape, unlocking countless
         opportunities for personal and professional growth.
       </p>
     </section>
     <section class="flex flex-col w-about-extra-custom">
-      <h4 class="txt-custom text-3xl font-bold text-white">
-        Who Can Benefit?
-      </h4>
+      <h4 class="txt-custom text-3xl font-bold text-white">Who Can Benefit?</h4>
       <p
         class="w-about-extra-custom txt-custom mt-2 mb-4 text-lg text-gray-300"
       >
         Our programs at Programmatics cater to anyone and are ideal for those
-        eager to immerse themselves in programming.
-        Parents who understand the significance of teaching their children with
-        early technological and problem-solving skills will discover our
-        offerings to be a valuable investment. Whether
-        your child is taking their first steps into coding or already has some
-        coding experience, our curriculum ensures they progress confidently and skillfully.
+        eager to immerse themselves in programming. Parents who understand the
+        significance of teaching their children with early technological and
+        problem-solving skills will discover our offerings to be a valuable
+        investment. Whether your child is taking their first steps into coding
+        or already has some coding experience, our curriculum ensures they
+        progress confidently and skillfully.
       </p>
     </section>
   </footer>
