a54bc4e8c95179983d963e6eef0b22bb8bc54582
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-08-27 10:57:56 -0400

    about changes

---
+++ src/components/About.vue
@@ -0,0 +1,27 @@
+<script setup>
+import AboutItem from './AboutItem.vue'
+
+</script>
+
+<template>
+  <AboutItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Mission</template>
+
+    Programmatic Blah blah blah
+  </AboutItem>
+
+  <AboutItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>The Team</template>
+
+        We are blabskdfalskdhfj
+
+  </AboutItem>
+
+
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,71 @@
+<script setup>
+import about from "../components/About.vue"
+</script>
+
 <template>
-  <header class="bg--about flex-col text-center shadow-lg">
+  <div class="container gap-96">
+    <div class="logo-container">
+      <img alt="Programmatic logo" class="logo" src="src/assets/logo.png" width="125" height="125" />
+    </div>
+    <header>
+      <main>
+        <div class="content">
+          <about />
+        </div>
+      </main>
+    </header>
+
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+  min-height: 100vh; 
+}
+
+header {
+  line-height: 1.5;
+  display: flex;
+  flex-direction: column;
+  align-items: center; 
+}
+
+.logo-container {
+  margin-top: auto; 
+  margin-bottom: auto; 
+}
+
+.logo {
+  display: block;
+}
+
+.content {
+  flex: 1; 
+}
+
+@media (max-width: 768px) {
+  .container {
+    flex-direction: column; 
+    align-items: stretch; 
+  }
+
+  header {
+    justify-content: center; 
+  }
+
+  .logo-container {
+    margin: 1rem 0; 
+  }
+}
+</style>
+
+
+
+
+  <!-- <header class="bg--about flex-col text-center shadow-lg">
     <h2
       class="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-blue-500 mb-3 font-extrabold text-8xl pb-2"
     >
@@ -67,36 +133,10 @@
         progress confidently and skillfully.
       </p>
     </section>
-  </footer>
-  <!-- <div class="black-box">
-    <div class="Aboutus-style">
+  </footer> -->
 
-      <h1 class="about-us-topic">What is Programmatic</h1>
-      <p class="about-us-info">
-        We are a nonprofit organization committed to providing extensive free online education in Scratch, Python,
-        Raspberry Pi, and Unity. Through Google Meet, we connect with students from diverse backgrounds and geographical
-        locations, offering comprehensive courses taught by experienced high school student instructors.</p>
-      <h1 class="about-us-topic">Our mission</h1>
-      <p class="about-us-info">At the heart of our organization lies a profound mission: to nurture digital literacy
-        among students and empower them with essential skills to not only navigate but thrive in an era fundamentally
-        defined by computers. We are dedicated to fostering a generation of individuals who can confidently embrace the
-        digital landscape, unlocking countless opportunities for personal and professional growth.
-      </p>
-      <h2 class="about-us-topic">Future goals</h2>
-      <p class="about-us-info">In our pursuit of empowering learners worldwide, our future goals encompass expanding our
-        teaching into other languages, ensuring accessibility to diverse communities, while also broadening the scope of
-        our curriculum to encompass a wider variety of programming languages. By breaking language barriers and offering
-        courses in multiple languages, we aim to create a more inclusive learning environment, enabling individuals from
-        different linguistic backgrounds to benefit from our expert instruction. Simultaneously, by incorporating a
-        diverse range of programming languages into our curriculum, we aspire to cater to the varied interests and
-        career aspirations of our students.
-      </p>
-      <div id="about-us-background-image"></div>
-    </div>
-  </div> -->
-</template>
 
-<style scoped>
+<!-- <style scoped>
 .bg--about {
   background-image: url("https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2662&q=80");
   background-repeat: no-repeat;
@@ -122,4 +162,4 @@
 .w-about-extra-custom {
   width: 400px;
 }
-</style>
+</style> -->
