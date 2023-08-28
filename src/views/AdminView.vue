3f8fbba37c652dff5632959653361fdb3776d463
ethqnol <112029223+ethqnol@users.noreply.github.com>
2023-08-28 15:47:31 -0400

    changes to admin

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,71 +1,152 @@
 <script setup>
 window.onload = function () {
 
-    const loginButton = document.getElementById('login-btn');
-    const usernameInput = document.getElementById('username');
-    const passwordInput = document.getElementById('password');
-    const messageElement = document.getElementById('message');
-    const main = document.getElementById('main')
-    const loginform = document.getElementById('login-form')
+    // const loginButton = document.getElementById('login-btn');
+    // const usernameInput = document.getElementById('username');
+    // const passwordInput = document.getElementById('password');
+    // const messageElement = document.getElementById('message');
+    // const main = document.getElementById('main')
+    // const loginform = document.getElementById('login-form')
 
-    loginButton.addEventListener('click', async (event) => {
-        event.preventDefault()
-        const user = usernameInput.value;
-        const pwd = passwordInput.value;
-       
-        const response = await fetch('http://localhost:5050/form/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ user, pwd })
+    // loginButton.addEventListener('click', async (event) => {
+    //     event.preventDefault()
+    //     const user = usernameInput.value;
+    //     const pwd = passwordInput.value;
+
+    //     const response = await fetch('http://localhost:5050/form/login', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({ user, pwd })
+    //     });
+    //     if (response.status === 200) {
+    //         messageElement.textContent = 'Login successful';
+    //         usernameInput.value = "";
+    //         passwordInput.value = "";
+    //         console.log("success")
+    //         main.classList.add("flex")
+    //         main.classList.remove("hidden")
+    //         loginform.classList.add("hidden")
+    //     } else {
+    //         messageElement.textContent = 'Login failed. Please check your credentials.';
+    //         passwordInput.value = "";
+    //     }
+    // });
+
+
+    const tableBody = document.querySelector('#datalistv tbody');
+    const tableBodyEnroll = document.querySelector('#tableliste tbody')
+    async function fetchData() {
+        const response = await fetch('http://localhost:5050/form/volunteer');
+        const data = await response.json();
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            const nameCell = document.createElement('td');
+            const emailCell = document.createElement('td');
+            const expertiseCell = document.createElement('td');
+            const selectedValuesCell = document.createElement('td');
+
+            nameCell.textContent = item.name || '';
+            emailCell.textContent = item.email || '';
+            expertiseCell.textContent = item.expertise || '';
+            selectedValuesCell.textContent = item.selectedValues.join(', ') || '';
+
+            row.appendChild(nameCell);
+            row.appendChild(emailCell);
+            row.appendChild(expertiseCell);
+            row.appendChild(selectedValuesCell);
+
+            tableBody.appendChild(row);
         });
-        if (response.status === 200) {
-            messageElement.textContent = 'Login successful';
-            usernameInput.value = "";
-            passwordInput.value = "";
-            console.log("success")
-            main.classList.add("flex")
-            main.classList.remove("hidden")
-            loginform.classList.add("hidden")
-        } else {
-            messageElement.textContent = 'Login failed. Please check your credentials.';
-            passwordInput.value = "";
-        }
-    });
+    }
+
+    async function fetchDataEnroll() {
+        const response = await fetch('http://localhost:5050/form');
+        const data = await response.json();
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            const nameCell = document.createElement('td');
+            const emailCell = document.createElement('td');
+            const enrolledCell = document.createElement('td');
+
+            nameCell.textContent = item.name || '';
+            emailCell.textContent = item.email || '';
+            enrolledCell.textContent = item.enrolled || '';
+
+            row.appendChild(nameCell);
+            row.appendChild(emailCell);
+            row.appendChild(enrolledCell);
+
+            tableBodyEnroll.appendChild(row);
+        });
+    }
+    // UNCOMMENT LATER
+    // fetchData() 
+    // fetchDataEnroll()
 }
 </script>
 
 <template>
-    <form id="login-form" class="bg-white p-8 shadow-md rounded-md w-80 mt-36 mb-36" >
+    <!-- <form id="login-form" class="bg-white p-8 shadow-md rounded-md w-80 mt-36 mb-36">
         <h1 class="text-2xl font-semibold mb-4 text-black">Authentication:</h1>
-        <input type="text" id="username" class="border rounded w-full py-2 px-3 mb-2 text-gray-700" placeholder="Username" />
-        <input type="password" id="password" class="border rounded w-full py-2 px-3 mb-4 text-gray-700" placeholder="Password" />
+        <input type="text" id="username" class="border rounded w-full py-2 px-3 mb-2 text-gray-700"
+            placeholder="Username" />
+        <input type="password" id="password" class="border rounded w-full py-2 px-3 mb-4 text-gray-700"
+            placeholder="Password" />
         <button type="submit" id="login-btn" class="bg-blue-500 text-white px-4 py-2 rounded w-full">Login</button>
-        <p id="message" class="text-red-500 mt-2"></p>
-    </form>
-    <main id="main" class="gap-x-80 top-44 hidden">
+        <p id="message" class="text-red-800"></p>
+    </form> -->
+    <main id="main" class="gap-x-80 top-44">
         <section class="fixed">
-            <h1 class="font-extrabold text-gray-200 text-6xl mt-32">Pass</h1>
+            <h1 class="font-extrabold text-gray-200 text-6xl mt-32">Enrolled</h1>
+            <!-- NOTE ADD HIDDEN CLASS WHEN USING LOGIN PAGE-->
 
+            <div class="row">
+                <div class="column">
+                    <table id="tableliste">
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Enrolled</th>
+                        <tr>
+                            <td>Jonathan</td>
+                            <td>Jonathan@fakkemail.nett</td>
+                            <td>Unity</td>
+                        </tr>
+                        <tr>
+                            <td>Rachel</td>
+                            <td>rachel@fakkemail.nett</td>
+                            <td>PyBasic</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </section>
         <hr class="px-20" />
         <div class="border border-gray-600"></div>
         <section class="flex flex-col gap-20 mt-36 mb-10">
+            <h1 class="font-extrabold text-gray-200 text-6xl mt-32">Volunteer</h1>
             <div class="row">
                 <div class="column">
-                    <table>
+                    <table id="datalistv">
                         <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
+                            <th>Name</th>
                             <th>Email</th>
+                            <th>Experience</th>
+                            <th>Courses</th>
                         </tr>
                         <tr>
-                            <td>Test</td>
-                            <td>Test</td>
-                            <td>Test</td>
+                            <td>Jonathan</td>
+                            <td>Jonathan@fakkemail.nett</td>
+                            <td>Jonathan is a master hacker who hacks kids on roblox lmfao get rket</td>
+                            <td>PyAdv, PyBasic</td>
                         </tr>
-
+                        <tr>
+                            <td>Bob</td>
+                            <td>bob@fakkemail.nett</td>
+                            <td>Bob hacked your mom with a potato</td>
+                            <td>PyAdv, Unity</td>
+                        </tr>
                     </table>
                 </div>
             </div>
@@ -74,6 +155,19 @@ window.onload = function () {
     </main>
 </template>
 <style scoped>
+* {
+    box-sizing: border-box;
+}
+
+.row {
+    margin-left: -5px;
+    margin-right: -5px;
+}
+
+.column {
+    float: left;
+}
+
 * {
     box-sizing: border-box;
 }
@@ -100,7 +194,7 @@ table {
     border-collapse: collapse;
     border-spacing: 0;
     width: 100%;
-    border: 1px solid #ddd;
+    border: 5px solid #ddd;
 }
 
 th,
@@ -109,7 +203,8 @@ td {
     padding: 16px;
 }
 
-tr:nth-child(even) {
-    background-color: #f2f2f2;
+
+a:hover {
+    text-decoration: underline;
 }
 </style>
