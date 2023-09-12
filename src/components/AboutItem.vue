51351581457edfec1cad927b4f555269a4034346
Ofish <vc.ofir@gmail.com>
2023-09-11 21:39:22 -0400

    IM FUCKING GOING TO DELETE OUR ENTIRE ABOUT

--- src/components/AboutItem.vue
+++ src/components/AboutItem.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="item">
-      <i>
-        <slot name="icon"></slot>
-      </i>
-      <div class="details">
-        <h3>
-          <slot name="heading"></slot>
-        </h3>
-        <slot></slot>
-      </div>
+  <div class="item">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <div class="details">
+      <h3>
+        <slot name="heading"></slot>
+      </h3>
+      <slot></slot>
     </div>
-  </template>
-  
-  <style scoped>
-  .item {
+  </div>
+</template>
+
+<style scoped>
+.item {
   margin-top: 2rem;
   display: flex;
   position: relative;
@@ -41,7 +41,7 @@ h3 {
   color: var(--color-heading);
 }
 
-@media (min-width: 1024px) {
+@media (min-width: 1025px) {
   .item {
     margin-top: 0;
     padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
@@ -59,7 +59,7 @@ h3 {
   }
 
   .item:before {
-    content: ' ';
+    content: " ";
     border-left: 1px solid var(--color-border);
     position: absolute;
     left: 0;
@@ -68,7 +68,7 @@ h3 {
   }
 
   .item:after {
-    content: ' ';
+    content: " ";
     border-left: 1px solid var(--color-border);
     position: absolute;
     left: 0;
@@ -84,4 +84,4 @@ h3 {
     display: none;
   }
 }
-  </style>
+</style>
