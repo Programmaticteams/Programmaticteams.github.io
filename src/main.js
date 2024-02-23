d52f66c2427ca53361cf8894aa94741985dcc027
Ofish <vc.ofir@gmail.com>
2024-02-22 23:06:34 -0500

    w

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+import "./assets/main.css";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./style.css";
-import "./assets/main.css";
 
 const app = createApp(App);
 
